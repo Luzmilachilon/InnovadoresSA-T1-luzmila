5a1e4e5506ca4136cbec60ec2e84fceb5e51de3e
Luzmilachilon <chilonsangayluzmila4@gmail.com>
2026-04-29 20:18:08 -0500

    agrego atributos de la clase Trabajador

--- src/innovadoressa/Trabajador.java
+++ src/innovadoressa/Trabajador.java
@@ -1,6 +1,15 @@
 package innovadoressa;
 
+
 public class Trabajador {
 
-    
+    private String nombre;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private String regimen;
+    private String fondoPension;
+    private double sueldoBase;
+    private boolean tieneHijos;
+    private boolean trabajaNocturno;
+    private boolean pactoColectivo;
 }
