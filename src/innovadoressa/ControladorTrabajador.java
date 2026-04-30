6c97b3a2b0c34b44d93fbeb22feb36eba9cef2f6
Luzmilachilon <chilonsangayluzmila4@gmail.com>
2026-04-29 20:33:47 -0500

    agrego controlador con lista y metodos agregar y listar

--- src/innovadoressa/ControladorTrabajador.java
+++ src/innovadoressa/ControladorTrabajador.java
@@ -1,8 +1,30 @@
 package innovadoressa;
 
+import java.util.ArrayList;
 
 public class ControladorTrabajador {
 
-    
-    
+    private ArrayList<Trabajador> listaTrabajadores;
+
+    public ControladorTrabajador() {
+        this.listaTrabajadores = new ArrayList<>();
+    }
+
+    public void agregarTrabajador(Trabajador t) {
+        listaTrabajadores.add(t);
+        System.out.println("Trabajador " + t.getNombre() + " registrado correctamente.");
+    }
+
+    public void listarTrabajadores() {
+        if (listaTrabajadores.isEmpty()) {
+            System.out.println("No hay trabajadores registrados.");
+            return;
+        }
+        System.out.println("\n=== LISTA DE TRABAJADORES ===");
+        for (int i = 0; i < listaTrabajadores.size(); i++) {
+            System.out.println("Trabajador #" + (i + 1) + ":" + listaTrabajadores.get(i));
+            System.out.println("-----------------------------");
+        }
+        System.out.println("Total registrados: " + listaTrabajadores.size());
+    }
 }
